df9b0d32a42c76c031bec271658eae75848f6be2
Minhyuk Park <parkminem@gmail.com>
2023-04-04 10:42:46 +0900

    create posts

---
+++ vue3-posts/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>AboutView</h2>
+    <p>{{ $route.name }}</p>
+    <button class="btn btn-primary" @click="moveHome">Move Home</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const moveHome = () => {
+  router.push('/');
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue3-posts/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>HomeView</h2>
+    <p>{{ $route.name }}</p>
+    <button class="btn btn-primary" @click="() => $router.push('/about')">
+      Move About
+    </button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
